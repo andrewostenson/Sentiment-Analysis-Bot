c113bb48df7cf6ec459db4b74f70acfbcfe32512
andrewostenson <ostensonandrew@gmail.com>
2026-06-05 16:28:46 -0600

    Removed manual run and clear buttons, fixed chart filtering

--- app.py
+++ app.py
@@ -21,24 +21,6 @@ year_ago = today - datetime.timedelta(days=365)
 
 st.title("Job Market Sentiment Analysis and Health")
 
-cols = st.columns(3)
-
-with cols[0]:
-    if st.button("Run Scraper and Sentiment Analysis"):
-        # Run the scraper and sentiment analysis
-        start = time.perf_counter()
-        main.run()
-        end = time.perf_counter()
-        st.toast(f"Scraping and sentiment analysis completed in {end - start:.2f} seconds")
-
-with cols[1]:
-    # Clear all posts if the clear data button is clicked
-    if st.button("Clear Data"):
-        database.delete_all_posts()
-
-with cols[2]:
-     view = st.toggle("Advanced View")
-
 selected_sentiment = st.selectbox("Select a sentiment to filter by:", options=["All", "positive", "neutral", "negative"])
 
 date_range = st.date_input(
@@ -49,22 +31,19 @@ date_range = st.date_input(
 
 keyword_search = st.text_input("Search by title or source:")
 
-
-
 filtered_data = database.fetch_filtered_posts(
     start_date=date_range[0],
     end_date=date_range[1],
-    sentiment_filter=selected_sentiment if selected_sentiment != "All" else None,
+    sentiment_filter= None,
     keyword_filter=keyword_search if keyword_search else None
 )
+
+view = st.toggle("Advanced View")
 
 # Fetch data from the database and convert it to a DataFrame, then create a copy of the DataFrame for display purposes
 chart_data = filtered_data.copy()
 table_data = chart_data.copy()
 
-# Filter the DataFrame based on the selected sentiment
-if selected_sentiment != "All":
-    chart_data = chart_data[chart_data['sentiment'] == selected_sentiment]
 
 if not view:
      table_data = table_data.drop(columns=['id', 'confidence_pos', 'confidence_neu', 'confidence_neg', 'timestamp'])
